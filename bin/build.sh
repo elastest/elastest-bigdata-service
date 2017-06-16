c1f0b0fda77a5fcd603cb7245d2f10925c9ab690
Savas Gioldasis <savas.gioldasis@upstreamsystems.com>
2017-06-16 12:48:08 +0300

    DEL: Spark

--- bin/build.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-docker build -t sgioldasis/elastest-spark-base:2.1.0 spark/base
-docker build -t sgioldasis/elastest-spark-master:2.1.0 spark/master
-docker build -t sgioldasis/elastest-spark-worker:2.1.0 spark/worker
