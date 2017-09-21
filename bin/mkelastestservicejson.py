53f8aec24e22d2cd8037219b92977ff4a985dbb9
Savas Gioldasis <savas.gioldasis@upstreamsystems.com>
2017-09-21 10:33:04 +0300

    CHG: elastestervice.json

--- bin/mkelastestservicejson.py
+++ bin/mkelastestservicejson.py
@@ -3,4 +3,7 @@ with open('elastestgenin.txt') as f, open('elastestgenout.txt', 'w') as o:
         o.write(line
         .replace('\n', '\\n')
         .replace('"', '\\"')
+        .replace('image: elastest/', 'image: et/')
+        .replace('elastest', 'elastest_elastest')
+        .replace('image: et/', 'image: elastest/')
         )
