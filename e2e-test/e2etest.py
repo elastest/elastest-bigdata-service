8e47e2f6077038f37207349b9d89753964cc5cf0
kmosch <46747178+kmosch@users.noreply.github.com>
2019-03-11 12:03:07 +0200

    Update e2etest.py

--- e2e-test/e2etest.py
+++ e2e-test/e2etest.py
@@ -17,17 +17,17 @@ print(json.loads(res.text))
 COMMANDS = """
 git clone https://github.com/elastest/demo-projects.git
 cd demo-projects/ebs-test
-mvn package
+mvn -q package
 rm -f big.txt
-wget https://norvig.com/big.txt
-#clean the pre-existing file
-hadoop fs  -rm /out.txt
+wget -q https://norvig.com/big.txt
+hadoop fs -rmr /out.txt 
 hadoop fs -rm /big.txt
 hadoop fs -copyFromLocal big.txt /big.txt
+
 spark-submit --class org.sparkexample.WordCountTask --master spark://sparkmaster:7077 /demo-projects/ebs-test/target/hadoopWordCount-1.0-SNAPSHOT.jar /big.txt
 hadoop fs -getmerge /out.txt ./out.txt
+
 head -20 out.txt
-ls
 """
 
 tjob=json.dumps({ "id": 0,
