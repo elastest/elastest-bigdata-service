d07641ef7457f02dfd81259f904d71b7084078b8
kmosch <46747178+kmosch@users.noreply.github.com>
2019-06-24 16:49:19 +0300

    Update seleniume2e.py

--- e2e-test/seleniume2e.py
+++ e2e-test/seleniume2e.py
@@ -35,7 +35,7 @@ capabilities = options.to_capabilities()
 eusUrl=os.environ['ET_EUS_API']
 print("EUS URL is: "+str(eusUrl))
 driver = webdriver.Remote(command_executor=eusUrl, desired_capabilities=capabilities)
-
+driver.get(url)
 # create new project
 time.sleep(5)
 element=driver.find_element_by_xpath("//button[contains(string(), 'New Project')]")
