508a3c60dbd1af9c9442b17d4cd3be6cf310b4bf
kmosch <46747178+kmosch@users.noreply.github.com>
2019-07-02 17:56:11 +0300

    Update seleniume2e.py

--- e2e-test/seleniume2e.py
+++ e2e-test/seleniume2e.py
@@ -69,7 +69,7 @@ while TSS_MAX_WAIT > 0:
 	try:
 		element = driver.find_element_by_id('resultMsgText')
 		print("\t TJob Execution Result: "+element.text)
-		if (element.text="Executing Test"):
+		if (element.text=="Executing Test"):
 			print("\t Waiting for tjob execution to complete")
 			time.sleep(20)
 			TSS_MAX_WAIT = TSS_MAX_WAIT - 20
