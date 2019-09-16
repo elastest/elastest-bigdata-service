82ca405570517ba1d7dc1d5b4471c06b40361948
kmosch <46747178+kmosch@users.noreply.github.com>
2019-09-16 15:35:52 +0300

    Update seleniume2e.py

--- e2e-test/seleniume2e.py
+++ e2e-test/seleniume2e.py
@@ -34,7 +34,7 @@ options.add_argument('--no-sandbox')
 capabilities = options.to_capabilities()
 eusUrl=os.environ['ET_EUS_API']
 print("EUS URL is: "+str(eusUrl))
-print(options)
+print(capabilities)
 driver = webdriver.Remote(command_executor=eusUrl, desired_capabilities=capabilities)
 driver.get(url)
 # create new project
